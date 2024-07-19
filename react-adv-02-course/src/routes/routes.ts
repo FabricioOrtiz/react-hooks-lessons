0314ffcf4c76773604f53f874a56fc2491e51588
Tito Ortiz CR <fabricio.ortiz.alfaro@gmail.com>
2024-07-19 12:35:52 -0600

    adding change for lazy loading and normal loading page

--- react-adv-02-course/src/routes/routes.ts
+++ react-adv-02-course/src/routes/routes.ts
@@ -1,16 +1,18 @@
-import { lazy } from "react";
+import { LazyExoticComponent, lazy } from "react";
 import { LazyPage1, LazyPage2, LazyPage3 } from "../pages/01-lazyload";
+
+type JSXComponent = () => JSX.Element;
 
 interface Route {
     to: string,
     path: string,
-    Component:React.LazyExoticComponent<() => JSX.Element>
+    Component:LazyExoticComponent<JSXComponent> | JSXComponent
     name: string
 }
 
 const Lazy1 = lazy(() => import('../pages/01-lazyload/LazyPage01'));
 const Lazy2 = lazy(() => import('../pages/01-lazyload/LazyPage02'));
-const Lazy3   = lazy(() => import('../pages/01-lazyload/LazyPage03'));
+const Lazy3 = lazy(() => import('../pages/01-lazyload/LazyPage03'));
 
 export const routes: Route[] = [
     {
@@ -28,7 +30,7 @@ export const routes: Route[] = [
     {
         to: '/lazy3',
         path: 'lazy3',
-        Component: Lazy3,
+        Component: LazyPage3,
         name: 'lazy3'
     }
 ]
